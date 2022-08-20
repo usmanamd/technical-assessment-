6cc80fcf1720a26f7880924bb99fa657869eec36
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-08-20 17:18:40 +0100

    first commit

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import Logo from "./logo/logo";
+import fetchData from "../dataFetcher";
+import Scorecard from "./Scorecard";
+import "./Scoreboard.css";
+import PartyLinks from "./PartyLinks";
+import { candidateData } from "../fakeAPI";
+
+function Scoreboard() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [results, setResults] = useState([]);
+
+  async function getData() {
+    try {
+      setLoading(true);
+      const resultData = await fetchData();
+      setResults(resultData.results);
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      setError(true);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="Scoreboard">
+      <header className="Election-scoreboard-header">
+        <Logo language="en" />
+      </header>
+      <main>
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : error ? (
+          <h1>Error</h1>
+        ) : (
+          <>
+            <h1>Results</h1>
+            <Scorecard results={results} candidateData={candidateData} />
+            <a className="Scoreboard-refresh" onClick={() => getData()}>
+              Refresh
+            </a>
+            <h1>Learn more about the parties...</h1>
+            <PartyLinks />
+          </>
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default Scoreboard;
